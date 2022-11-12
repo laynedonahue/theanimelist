d09220ba83f3ce8d747fc6b4ab7169999c2a0778
Chad <chadndonahue@gmail.com>
2022-11-12 13:15:15 -0800

    added a sidebar for the top anime and added styling for it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import Header from "./components/Header";
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import {useState, useEffect} from 'react'
 
 
 
 function App() {
+  const [animeList, setAnimeList] = useState([]);
+  const [topAnime, setTopAnime] = useState([]);
+  const [search, setSearch] = useState('');
+
   return (
     <div className="App">
       <Header/>
+      <div className='content-wrap'>
+          <Sidebar topAnime={topAnime}/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <aside>
+        <nav>
+            <h3>Top Anime</h3>
+            <a href='#' target='_blank' rel='noreferrer'>JoJo's Bizarre Adventure</a> 
+            <a href='#' target='_blank' rel='noreferrer'>Haikyuu!!</a> 
+            <a href='#' target='_blank' rel='noreferrer'>So I'm a Spider, So What?</a> 
+        </nav>
+      
+    </aside>
+  )
+}
+
+export default Sidebar
